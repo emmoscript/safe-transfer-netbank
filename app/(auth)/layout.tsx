f3a2ee8496e226289e1659c46645018face0f240
Emil0309 <71798519+Emil0309@users.noreply.github.com>
2024-10-20 12:08:07 -0400

    Implement auth, appwrite and sentry

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,4 @@
-
+import Image from "next/image";
 
 export default function RootLayout({
     children,
@@ -6,8 +6,19 @@ export default function RootLayout({
     children: React.ReactNode;
   }>) {
     return (
-      <main>
+      <main className="flex min-h-screen w-full justify-between font-inter">
           {children}
+          <div className="auth-asset">
+            <div>
+              <Image
+                src="/icons/auth-image.svg"
+                alt="Auth image"
+                width={900}
+                height={900}
+                className="rounded-l-xl object-contain"
+              />
+            </div>
+          </div>
       </main>
     );
   }
